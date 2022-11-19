0464bcd27a9c4f8a1731b852f5a321d2738e9360
Joseph Kiwan <49622042+josephkiwan@users.noreply.github.com>
2022-11-19 03:38:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php require('./inc/head.php'); ?>
+
+  <body class="p-0 m-0">
+    <div class="overlay-message d-none">
+        <div class="position-absolute" style="z-index: 999; left: 320px; top: -130px;"><img src="assets/images/graphic-3.png" /></div>
+        <div class="position-absolute" style="z-index: 999; left: -30px; top: 193px;"><img src="assets/images/graphic-2.png" /></div>
+        <div class="overlay-message-container">
+            <div class="__heading_1">The Foundation is</div>
+            <div class="__heading_2">now open!</div>
+            <div class="__content">
+                <div class="__line_1">Based in Beirut's iconic Corm building, our renovated grounds are now open to the public after 90 years,</div>
+                <div class="__line_2">to serve as a multiuse house to host artists, activists and innovators.</div>
+                <div class="__flex">
+                    <a href="">Discover Our Story</a>
+                    <a href="">Book A Visit</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require('./inc/header.php'); ?>
+
+    <div class="header-spacer-150"></div>
+
+    <div class="container-fluid p-0 m-0 hero_container">
+        <div class="position-absolute" style="z-index: 999; top: 50%; overflow: hidden;"><img src="assets/images/graphic-1.png" /></div>
+        <div class="position-absolute" style="z-index: 999; right: 0px; top: 40px; overflow: hidden;"><img src="assets/images/graphic-4.png" /></div>
+		<div class="swiper swiperHero">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+					<div class="slide_container slide-1">
+						<div class="slide_wrapper">
+							<div class="slide_content">
+								<h2>At The Foundation Now</h2>
+								<h1>The French Institute Annual Book Fair</h1>
+                                <div class="date">October 26 - December 10, 2022</div>
+                                <a href="">Explore Our Opening Exhibition</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-slide">
+					<div class="slide_container slide-1">
+						<div class="slide_wrapper">
+							<div class="slide_content">
+								<h1>At The Foundation Now</h1>
+								<h2>The French Institute Annual Book Fair</h2>
+                                <div>October 26 - December 10, 2022</div>
+                                <a href="">Explore Our Opening Exhibition</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="swiper-pagination swiperHero-pagination"></div>
+			<div class="swiper-button-prev swiperHero-button-prev"></div>
+			<div class="swiper-button-next swiperHero-button-next"></div>
+			
+			<div class="sub_menu_container">
+				<div class="sub_menu_wrapper">
+					<a href="#info">über den notfallsticker</a>
+					<a href="#hiw">so funktioniert's</a>
+					<a href="#faq">faq</a>
+					<a href="#order">Notfallsticker bestellen</a>
+				</div>
+			</div>
+		</div>
+    </div>
+
+    <div class="container">
+        <div class="row d-flex align-items-end pt-5 pb-5">
+            <div class="col-7">
+                <h1>Nous sommes une fondation à but non-lucratif, guidée par la vision humaniste du poète et entrepreneur Charles Corm —</h1>
+                <h2>fondée pour promouvoir et soutenir le patrimoine culturel et naturel du Liban.</h2>
+                <p class="p-0 m-0">Nous sommes basés  dans la maison Corm. Cette tour blanche reconnaissable de par son architecture, sera ouverte au public pour la première fois en 90 ans. </p>
+                <p class="p-0 m-0">Transformée en espace polyvalent, elle accueillera bientôt artistes, chercheurs et innovateurs.</p>
+            </div>
+            <div class="col-5">
+                <a href="">Our Mission</a>
+                <a href="">About Charles Corm</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 class="pb-3">What's On</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row d-flex align-items-center">
+            <div class="col-7 m-0 p-0">
+                <img src="assets/images/image_1.png" class="w-100 image-fluid" />
+            </div>
+            <div class="col-3">
+                <h3>Upcoming Events</h3>
+                <h2>Eco-Friendly Spheres Of Sand To Debut At Downtown Dubai Design Fair</h2>
+                <p>November 09 - 12, 2022</p>
+                <p>We are proud to announce our support of Lebanese design duo Karim+Elias, who have been selected to design the fair's entrance installation in accordance with their mission towards sustainability.</p>
+                <a href="">Lire La Suite</a>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row d-flex align-items-center">
+            <div class="col-2"></div>
+            <div class="col-3">
+                <h3>Upcoming Events</h3>
+                <h2>Eco-Friendly Spheres Of Sand To Debut At Downtown Dubai Design Fair</h2>
+                <p>November 09 - 12, 2022</p>
+                <p>We are proud to announce our support of Lebanese design duo Karim+Elias, who have been selected to design the fair's entrance installation in accordance with their mission towards sustainability.</p>
+                <a href="">Lire La Suite</a>
+            </div>
+            <div class="col-7 m-0 p-0">
+                <img src="assets/images/image_2.png" class="w-100 image-fluid" />
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row d-flex align-items-center">
+            <div class="col-7 m-0 p-0">
+                <img src="assets/images/image_3.png" class="w-100 image-fluid" />
+            </div>
+            <div class="col-3">
+                <h3>Upcoming Events</h3>
+                <h2>Eco-Friendly Spheres Of Sand To Debut At Downtown Dubai Design Fair</h2>
+                <p>November 09 - 12, 2022</p>
+                <p>We are proud to announce our support of Lebanese design duo Karim+Elias, who have been selected to design the fair's entrance installation in accordance with their mission towards sustainability.</p>
+                <a href="">Lire La Suite</a>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row ">
+            <div class="col-sm-4">
+                <h1>Our Initiatives</h1>
+            </div>
+            <div class="col-sm-4">
+                <span class="align-middle">View All</span>
+            </div>
+        </div>
+    </div>
+    <div class="container mb-5">
+        <div class="card-group">
+            <div class="card mr-2 border-0">
+              <img class="card-img-top" src="assets/images/graphic.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">The Phoenician Heritage Institute</h5>
+                <h5 class="card-title">Light On The Phoenicians</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Read More</small></p>
+              </div>
+            </div>
+            <div class="card mr-2 border-0">
+            <img class="card-img-top" src="assets/images/graphic.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Read More</small></p>
+              </div>
+            </div>
+            <div class="card mr-2 border-0">
+            <img class="card-img-top" src="assets/images/graphic.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Read More</small></p>
+              </div>
+            </div>
+          </div>
+    </div>
+
+    <?php require('./inc/footer.php'); ?>
